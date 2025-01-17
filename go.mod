2bcc7ac4c8ed15c9b1cca54ed390b5582281e3aa
Trung Le <trungdoanle1101@gmail.com>
2025-01-16 21:26:00 -0600

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/trungdoanle1101/server
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
