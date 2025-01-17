e30fce439943f9caf45e37c4d2c5a8d46633fdb9
Trung Le <trungdoanle1101@gmail.com>
2025-01-17 15:15:26 -0600

    add json

--- main.go
+++ main.go
@@ -2,17 +2,66 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	handlerFunc := func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(handlerFunc)
+}
+
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
 	mux := http.NewServeMux()
-	server := http.Server{
+	apiCfg := &apiConfig{}
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	server := &http.Server{
 		Handler: mux,
-		Addr:    ":8080",
+		Addr:    ":" + port,
 	}
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println(err)
-	}
+	log.Fatal(server.ListenAndServe())
+
+}
+
+const metricsHtml = `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	status := http.StatusOK
+	content := fmt.Sprintf(metricsHtml, cfg.fileserverHits.Load())
+	w.WriteHeader(status)
+	w.Write([]byte(content))
+
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	var status int
+	content := ""
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHits.Store(0)
+	status = http.StatusOK
+	content = "Hits reset to zero"
+	w.WriteHeader(status)
+	w.Write([]byte(content))
 }
